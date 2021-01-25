8086726fd76d46318b207953130e86696a6fba9f
barthelemy GODART <barthelemy.godart11@gmail.com>
2021-01-25 09:03:57 +0100

    okBoy

--- templates/accueil.php
+++ templates/accueil.php
@@ -1,5 +1,4 @@
 <div>
-   <?php //readfile(getcwd().'/templates/carouselTrendPublication.html');?>
-   <?php //readfile(getcwd().'/templates/carouselTrendHashtag.html');?>
-   <?php readfile(getcwd().'/templates/carouselOfficielHashtag.html');?>
+   <?php readfile(getcwd().'/templates/carouselTrendPublication.html');?>
+   
 </div>
