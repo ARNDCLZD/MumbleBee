476bd54fb4c4112060ccdea8ed8c576411adf60e
ARNDCLZD <64208880+ARNDCLZD@users.noreply.github.com>
2021-01-25 09:02:58 +0100

    CSS Accueil DERNIER

--- templates/accueil.php
+++ templates/accueil.php
@@ -1,5 +1,6 @@
-<div>
-   <?php //readfile(getcwd().'/templates/carouselTrendPublication.html');?>
-   <?php //readfile(getcwd().'/templates/carouselTrendHashtag.html');?>
-   <?php readfile(getcwd().'/templates/carouselOfficielHashtag.html');?>
+<div class="flex grid grid-cols-1">
+    <div class="flex justify-center items-center text-center bg-noir-800">
+        <h1 class="p-12 text-4xl text-jaune-300 font-bold">Bienvenue sur Mumble Bee.</h1>
+    </div>
+   <?php readfile(getcwd().'/templates/carouselTrendHashtag.html');?>
 </div>
